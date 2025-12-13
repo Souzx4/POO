9b3380005e37b7a2212331af29b76e6c8ca244e2
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-12-12 22:59:29 -0300

    UFC

---
+++ UFC/src/main/java/com/mycompany/ufc/Luta.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ufc;
+
+import java.util.Random;
+
+/**
+ *
+ * @author gilma
+ */
+public class Luta {
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+    
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        } else {
+            this.aprovada = false;
+            this.desafiante = null;
+            this.desafiado = null;   
+        }
+    }
+    public void lutar(){
+        if (this.aprovada){
+            System.out.println("### Desafiado ###");
+            this.desafiado.apresentar();
+            System.out.println("### Desafiante ###");
+            this.desafiante.apresentar();
+            
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3); // 0 1 2
+            
+            switch(vencedor){
+                case 0: // empate
+                    System.out.println("empatou");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1: //ganhou desafiado
+                    System.out.println("Vitoria do " + this.desafiado.getNome());
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2: //ganhou desafiante
+                    System.out.println("Vitoria do " + this.desafiante.getNome());
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                    break;
+            }       
+        } else {
+            System.out.println("Luta não pode acontecer");
+        }
+    }
+
+    public Luta() {
+        this.desafiado = null;
+        this.desafiante = null;
+        this.rounds = 0;
+        this.aprovada = false;
+    }
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
+}
--- UFC/src/main/java/com/mycompany/ufc/UFC.java
+++ UFC/src/main/java/com/mycompany/ufc/UFC.java
@@ -14,28 +14,14 @@ public class UFC {
         Lutador l[] = new Lutador[6];
                 
         l[0] = new Lutador(" Lionel Messi", "Argentina", 38, 1.7f, 67f, 766, 153, 218);
-        l[1] = new Lutador(" Cristiano Ronaldo", "Portugal", 40, 1.97f, 83f, 800, 201, 240);
+        l[1] = new Lutador(" Cristiano Ronaldo", "Portugal", 40, 1.97f, 70f, 800, 201, 240);
         l[2] = new Lutador(" Yuri Alberto", "Brasil", 24, 1.83f, 78f, 186, 97, 95);
         l[3] = new Lutador(" Arrascaeta", "Uruguai",31, 1.74f, 68f, 403, 115, 142);
         l[4] = new Lutador(" Mbappe", "França",26, 1.85f, 75f, 390, 70, 90);
         l[5] = new Lutador(" Neymar", "Brasil",33, 1.75f, 68f, 480, 120, 100);
         
-        l[0].apresentar();
-        l[0].status();
-        
-        l[1].apresentar();
-        l[1].status();
-        
-        l[2].apresentar();
-        l[2].status();
-        
-        l[3].apresentar();
-        l[3].status();
-        
-        l[4].apresentar();
-        l[4].status();
-        
-        l[5].apresentar();
-        l[5].status();
+        Luta x = new Luta();
+        x.marcarLuta(l[0], l[1]);
+        x.lutar();
     }
 }
