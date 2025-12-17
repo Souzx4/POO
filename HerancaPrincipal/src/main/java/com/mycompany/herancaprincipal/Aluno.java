b1d0f9cfe0fc4714fb3dcad1b6bc35c8cfb40058
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2025-12-17 12:11:59 -0300

    up

---
+++ HerancaPrincipal/src/main/java/com/mycompany/herancaprincipal/Aluno.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.herancaprincipal;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class Aluno extends Pessoa {
+    private String matr;
+    private String curso;
+    
+
+    public Aluno(String nome, int idade, String sexo) {
+        super(nome, idade, sexo);
+    }
+    
+    public void cancelarMatr(String Pessoa){
+        if (this.matr.equals("Ativa")){
+            this.matr.equals("Cancelar");
+            System.out.println("Matricula de " + this.getNome() + " foi cancelada");
+        } else {
+            System.out.println("A matricula está ativa");
+        }
+    }
+
+    public Aluno(String matr, String curso, String nome, int idade, String sexo) {
+        super(nome, idade, sexo);
+        this.matr = matr;
+        this.curso = curso;
+        
+    }
+
+    public String getMatr() {
+        return matr;
+    }
+
+    public void setMatr(String matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    
+    
+    
+}
---
+++ HerancaPrincipal/src/main/java/com/mycompany/herancaprincipal/Funcionario.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.herancaprincipal;
+
+/**
+ *
+ * @author Eletel_Monitor
+ */
+public class Funcionario extends Pessoa {
+    
+    public Funcionario(String nome, int idade, String sexo) {
+        super(nome, idade, sexo);
+    }
+    
+}
