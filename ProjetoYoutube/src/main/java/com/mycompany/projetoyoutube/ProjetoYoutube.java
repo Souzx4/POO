c807fb5cc87caae8caf4c336163bb02f6fbe1fa3
Gilmar Sousa De Brito <gilmarsousa717@gmail.com>
2026-01-05 13:53:31 -0300

    up

---
+++ ProjetoYoutube/src/main/java/com/mycompany/projetoyoutube/ProjetoYoutube.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoyoutube;
+
+/**
+ *
+ * @author Eletel_Atendimento
+ */
+public class ProjetoYoutube {
+
+    public static void main(String[] args) {
+        Video v[] = new Video[2];
+        v[0] = new Video("Aula 1 de POO");
+        v[1] = new Video("Aula 2 de POO");
+        System.out.println(v[0].toString());
+    }
+}
---
+++ ProjetoYoutube/src/main/java/com/mycompany/projetoyoutube/Video.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetoyoutube;
+
+/**
+ *
+ * @author Eletel_Atendimento
+ */
+public class Video implements AcoesVideos {
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzidas;
+
+    public Video(String titulo) {
+        this.titulo = titulo;
+        this.avaliacao = 1;
+        this.views = 0;
+        this.curtidas = 0;
+        this.reproduzidas = false;
+    }    
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzidas() {
+        return reproduzidas;
+    }
+
+    public void setReproduzidas(boolean reproduzidas) {
+        this.reproduzidas = reproduzidas;
+    }
+    
+    
+    
+    @Override
+    public void play() {
+        
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void like() {
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" + "titulo=" + titulo + ", avaliacao=" + avaliacao + ", views=" + views + ", curtidas=" + curtidas + ", reproduzidas=" + reproduzidas + '}';
+    }
+    
+    
+    
+}
